b97b9dc54c271793474330179dcf984e4870122b
kellanyamamoto1 <157436045+kellanyamamoto1@users.noreply.github.com>
2024-03-18 17:00:41 -0700

    Update ui.py

--- ui.py
+++ ui.py
@@ -1,23 +1,33 @@
 from pathlib import Path
-#import user as use
+import user as user_mod
 from Profile import Profile, Post
 from ds_client import send
+import administration as admin
+import ui as ui
+import a4 as a4
+import OpenWeather as WEA
+import LastFM as LFM
 
 server_adress = "168.235.86.101"
 server_port = "3021"
 administrator = False
 temp_path = ''
 
-
+def start():
+    ui.administration(1)
+    ui.command()
 
 
 def user():
+    global administrator
     user_type = input("Please input user type (\"admin\", \"user\"):  ")
     temp = 0
     if user_type == 'admin':
         temp = 1
+        administrator = True
     else:
         temp = 0
+        administrator = False
     return temp
 
 
@@ -30,33 +40,38 @@ def administration(a):
     return administrator
 
 
-def comm():
+def command():
     command = input("please enter command:  ")
     command_type = command[0:1]
     if command == 'admin':
         admin.start()
+    elif command == 'user':
+        administrator == 0
+        user()
+        command()
     else:
         if command_type == "L":
-            list_files(command)
+            list_files_command(command)
         elif command_type == "Q":
             quit()
         elif command_type == "C":
             create_file(command)
-            comm()
+            command()
         elif command_type == "D":
             del_file(command)
         elif command_type == "R":
             read_file(command)
         elif command_type == "H":
-            use.comm_list()
-            comm()
+            user_mod.comm_list()
+            command()
         elif command_type == "O":
             open_file(command)
-            comm()
+            command()
         elif command_type == 'E':
             edit_file(command)
         elif command_type == "P":
             print_data(command)
+        
 
 
 def file_sort(a, b):
@@ -94,17 +109,35 @@ def list_files(a):
         else:
             if administrator:
                 print("please enter a valid path")
-                comm()
+                command()
             else:
-                use.path_help()
+                user_mod.path_help()
     else:
-        path = use.get_path()
-        recursive = use.recursive()
-        files_only = use.files()
-        ending = use.ending()
-        search_file = use.search()
+        path = user_mod.get_path()
+        recursive = user_mod.recursive()
+        files_only = user_mod.files()
+        ending = user_mod.ending()
+        search_file = user_mod.search()
         print("OUTPUT:\n")
         list_items(path, recursive, files_only, search_file, ending)
+
+def list_files_command(command):
+    command_parts = command.split()
+    paths = command_parts[1:]
+    path = paths[0]
+    recursive = "-r" in paths
+    files_only = "-f" in paths
+    search_file = None
+    if "-s" in paths:
+        s_index = paths.index("-s")
+        if s_index + 1 < len(paths):
+            search_file = paths[s_index + 1]
+    ending = None
+    if "-e" in paths:
+        e_index = paths.index("-e")
+        if e_index + 1 < len(paths):
+            ending = paths[e_index + 1]
+    list_items(path, recursive, files_only, search_file, ending)
 
 
 def list_items(path, recursive=False, files_only=False, search_file=None, ending=None):
@@ -136,7 +169,7 @@ def list_items(path, recursive=False, files_only=False, search_file=None, ending
                 print(directory)
     except FileNotFoundError:
         print(f"the path {path} doesnt exist")
-    comm()
+    command()
 
 
 def create_file(a):
@@ -161,12 +194,11 @@ def create_file(a):
                 profile.save_profile(path = filepath)
                 print(f'{filepath} OPENED')
                 temp_path = filepath
-        print(f"this is the way  {temp_path}")
-        return temp_path
-                
+        print(f"TEMP PATH:  {temp_path}")
+        return temp_path        
     else:
-        file_path = use.get_path()
-        file_name = use.file_name()
+        file_path = user_mod.get_path()
+        file_name = user_mod.file_name()
         line = file_path + "\\" + file_name
         username = input("Enter Username:  ")
         password = input("Enter Password:  ")
@@ -208,17 +240,17 @@ def del_file(a):
         else:
             print("can only delete dsu files")
     else:
-        path = use.get_path()
+        path = user_mod.get_path()
         if path[-3:] == 'dsu':
             if check_file(path):
                 Path(path).unlink()
                 print(f"{path} DELETED")
             elif not check_file(path):
-                print("no such file exists")
+                print("no file exists")
         else:
             print("can only delete dsu files")
 
-    comm()
+    command()
 
 
 def read_file(a):
@@ -238,7 +270,7 @@ def read_file(a):
                 print("no such file exists")
         print("")
     else:
-        path = use.get_path()
+        path = user_mod.get_path()
         if path[-3:] == 'dsu':
             if check_file(path):
                 with open(path, 'r') as p:
@@ -249,11 +281,11 @@ def read_file(a):
                     else:
                         print("EMPTY")
             elif not check_file(path):
-                print("no such file exists") 
+                print("no file exists") 
         else:
             print("please enter a file with \".dsu\" extention")
     print("")
-    comm()
+    command()
 
 
 def open_file(a):
@@ -262,19 +294,19 @@ def open_file(a):
         path = a.split(' ')
         temp_path = path[1]
         f = open(temp_path, 'a')
-        print(temp_path + " has been opened")
+        print(temp_path + " opened")
         return temp_path
     else:
-        path = use.get_path()
+        path = user_mod.get_path()
         print("Without the file extention,")
-        name = use.file_name()
+        name = user_mod.file_name()
         temp_path = path + "\\" + name
         f = open(temp_path, 'r+')
-        print(temp_path + ' Has been opened')
+        print(temp_path + ' opened')
         for line in f:
-            print("hehehehe")
+            print(" ")
             print(line.strip())
-    comm()
+    command()
     return temp_path
         
 
@@ -292,10 +324,16 @@ def edit_file(a):
         profile = Profile()
         profile.load_profile(path = temp_path)
         
-        if '-usr' in lis:
-            usr_index = lis.index('-usr')
+        if '-user' in lis:
+            usr_index = lis.index('-user')
             new_usr = ' '.join(lis[usr_index + 1:]).strip('"')
             profile.username = new_usr
+            profile.save_profile(temp_path)
+        if '-addpost' in lis:
+            post_index = lis.index('-addpost')
+            post_content = ' '.join(lis[post_index + 1:])
+            new_post = Post(post_content)
+            profile.add_post(new_post)
             profile.save_profile(temp_path)
         if '-pwd' in lis:
             pwd_index = lis.index('-pwd')
@@ -305,24 +343,18 @@ def edit_file(a):
         if '-bio' in lis:
             profile.bio = bio.strip('"')
             profile.save_profile(temp_path)
-        if '-addpost' in lis:
-            post_index = lis.index('-addpost')
-            post_content = ' '.join(lis[post_index + 1:])
-            new_post = Post(post_content)
-            profile.add_post(new_post)
-            profile.save_profile(temp_path)
     else:
         profile = Profile()
         print("please enter a dsu file path")
         temp_path = input()
         profile.load_profile(path = temp_path)
         print("what would you like to edit?")
-        print("\"-usr\" to update the username")
+        print("\"-user\" to update the username")
         print("\"-pwd\" to update password")
         print("\"-bio\" to update bio")
         print("\"-addpost\" to add a post")
         user_in = str(input())
-        if "-usr" in user_in:
+        if "-user" in user_in:
             new = str(input("enter new username: "))
             profile.username = new
             profile.save_profile(temp_path)
@@ -336,6 +368,23 @@ def edit_file(a):
             profile.save_profile(temp_path)
         elif "-addpost" in user_in:
             post_content = input("Enter new post: ")
+            if "@W" in post_content or "@w" in post_content:
+                zipc = input("Enter Zipcode: ")
+                cc = input("Enter Country Code: ")
+                Fire = WEA.OpenWeather(zipc, cc)
+                Fire.set_apikey("ceb8cbc931c2f41301ba4a1548020fd4")
+                Fire.load_data()
+                post_content = Fire.transclude(post_content)   
+            if "@L" in post_content or "@l" in post_content:
+                alb = input("Enter Album Song Album: ")
+                art = input("Enter Artist: ")
+                Water = LFM.LastFM()
+                Water.set_artist_album(art, alb)
+                Water.setFMapi("7cd2ee13dc3b0100dae94c5c7401df50")
+                Water.loadFMdata()
+                #print(Water.loadFMdata())
+                post_content = Water.transclude(post_content)
+                
             new_post = Post(post_content)
             profile.add_post(new_post)
             profile.save_profile(temp_path)
@@ -348,7 +397,7 @@ def edit_file(a):
                 message = post_content
                 send(serv, port, username, password, message)
 
-    comm()
+    command()
 
 
 def print_data(command):
@@ -382,8 +431,8 @@ def print_data(command):
         print("Posts:")
         for i, post in enumerate(profile._posts):
             print(f"  Post {i}: {post}")
-    comm()
+    command()
 
 if __name__ == "__main__":
-    user()
+    start()   
 
